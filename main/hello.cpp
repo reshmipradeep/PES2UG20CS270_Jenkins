b3d61682c02c994653e97ebd7bf967053eb63c0c
Resh <85751727+reshmipradeep@users.noreply.github.com>
2023-02-13 13:56:00 +0530

    Added req files

---
+++ main/hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(void) 
+{
+  cout << "Hello it's PES2UG20CS270\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run it.\n";
+  return 0;
+}
